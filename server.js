e1d450f3bfb1ea7e62683b99c579e369aed87cd9
JovandaTegarPamungkas <jovantegar191005@gmail.com>
2025-12-03 18:43:57 +0700

    update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const { Client } = require('pg');
+const db = require('./db.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
